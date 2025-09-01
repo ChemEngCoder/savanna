be584a41961bc6407d4b4ae613c96fb28351da5d
SpahlMC <mark.spahl@ucalgary.ca>
2025-09-01 12:43:14 -0400

    filetest fix 2

--- filetest.py
+++ filetest.py
@@ -1,4 +1,5 @@
-from os.path import join, listdir
+from os import listdir
+from os.path import join
 
 path = join("..", "..", "scratch", "evo", "pretraining_or_both", "gtdb_v220_imgpr")
 files = [f for f in listdir(path)]
