04901a8397b1b393402ec0bab48afd7cf69bfbbd
SpahlMC <mark.spahl@ucalgary.ca>
2025-09-10 11:12:53 -0400

    Removed leading slash from train filepath

--- filetest.py
+++ filetest.py
@@ -13,9 +13,9 @@ print(path)
 files = [f for f in listdir(path)]
 print(files)
 
-train_path = "/data/evo/pretraining_or_both/gtdb_v220_imgpr/data_gtdb_train_chunk1_text_CharLevelTokenizer_document"
-test_path = "/data/evo/pretraining_or_both/gtdb_v220_imgpr/data_gtdb_test_chunk1_text_CharLevelTokenizer_document"
-valid_path = "/data/evo/pretraining_or_both/gtdb_v220_imgpr/data_gtdb_valid_chunk1_text_CharLevelTokenizer_document"
+train_path = "data/evo/pretraining_or_both/gtdb_v220_imgpr/data_gtdb_train_chunk1_text_CharLevelTokenizer_document"
+test_path = "data/evo/pretraining_or_both/gtdb_v220_imgpr/data_gtdb_test_chunk1_text_CharLevelTokenizer_document"
+valid_path = "data/evo/pretraining_or_both/gtdb_v220_imgpr/data_gtdb_valid_chunk1_text_CharLevelTokenizer_document"
 
 i = 0
 
