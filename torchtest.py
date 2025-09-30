6a4f0560419863ce9540d2a53ecf999f596d82a0
SpahlMC <mark.spahl@ucalgary.ca>
2025-09-29 22:24:39 -0400

    Added print of cwd

--- torchtest.py
+++ torchtest.py
@@ -1,5 +1,6 @@
 import os
 
+print("CWD:", os.getcwd())
 print("VENV:", os.environ.get("VIRTUAL_ENV"))
 print("PYTHONNOUSERSITE:", os.environ.get("PYTHONNOUSERSITE"))
 print("LD_LIBRARY_PATH head:", os.environ.get("LD_LIBRARY_PATH","").split(":")[:3])
