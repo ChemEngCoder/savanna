be5f0dea115322a9b458428cd920c9064c5678e3
SpahlMC <mark.spahl@ucalgary.ca>
2025-09-29 22:17:50 -0400

    Added prints to torchtest.py

--- torchtest.py
+++ torchtest.py
@@ -1,3 +1,9 @@
+import os
+
+print("VENV:", os.environ.get("VIRTUAL_ENV"))
+print("PYTHONNOUSERSITE:", os.environ.get("PYTHONNOUSERSITE"))
+print("LD_LIBRARY_PATH head:", os.environ.get("LD_LIBRARY_PATH","").split(":")[:3])
+
 import torch
 
 print(torch.version.cuda)
