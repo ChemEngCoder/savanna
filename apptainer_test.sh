32a0186f32ebfeb0689137b596bac9c87b9e1f5b
SpahlMC <mark.spahl@ucalgary.ca>
2025-09-30 21:49:29 -0400

    wombo3

--- apptainer_test.sh
+++ apptainer_test.sh
@@ -37,7 +37,7 @@ apptainer exec --nv --cleanenv \
 
     # 5) optionally append any existing entries after filtering out stale torch paths
     LD_OLD_FILTERED=$(echo "${LD_LIBRARY_PATH:-}" | tr ':' '\n' | \
-    grep -vE 'python3\.10/.*/torch(_tensorrt)?/lib' | paste -sd: -)
+    grep -vE "python3\.10/.*/torch(_tensorrt)?/lib" | paste -sd: -)
     export LD_LIBRARY_PATH="${LD_NEW}${LD_OLD_FILTERED:+:$LD_OLD_FILTERED}"
 
     # (optional) quick print
